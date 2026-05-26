6148e4983fc14a6a01161dff9aaaeb3fc18316d3
Rep1ace <yang_le_han@outlook.com>
2026-05-26 21:38:46 +0800

    sm8750-common: add device-owned sensors multihal
    Change-Id: I52d40f47b900d89ee50b12fa42cae1d4682304bd

--- common.mk
+++ common.mk
@@ -304,7 +304,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-sm8750-multihal
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_sun/android.hardware.sensor.accelerometer.xml \
